4dd2680fa2bdcb203d62d5e97f1eb8c8eb38b5bf
jiangxch <jiangxch8355@fxiaoke.com>
2020-07-19 03:38:55 +0800

    修改sql

--- hasakei-config/src/main/resources/db/config.sql
+++ hasakei-config/src/main/resources/db/config.sql
@@ -1,52 +1,39 @@
-
-
-
 CREATE SCHEMA hasakei AUTHORIZATION hasakei;
 
 /******************************************/
 /*   数据库全名 = hasakei   */
 /*   表名称 = config_info   */
 /******************************************/
-CREATE TABLE `config_info`
+CREATE TABLE config_info
 (
-    `id`           bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `config_name`  varchar(255) NOT NULL COMMENT 'data_id',
-    `environment`  varchar(255) NOT NULL COMMENT '配置文件所属环境',
-    `content`      longtext     NOT NULL COMMENT 'content',
-    `md5`          varchar(32)  DEFAULT NULL COMMENT 'md5',
-    `gmt_create`   BIGINT(15)   NOT NULL COMMENT '创建时间',
-    `gmt_modified` BIGINT(15)   NOT NULL COMMENT '修改时间',
-    `config_desc`  varchar(256) DEFAULT NULL COMMENT '配置描述',
-    `user_id`      integer      NOT NULL,
-    `username`     varchar(255) NOT NULL,
+    id           bigint(20) NOT NULL AUTO_INCREMENT ,
+    config_name  varchar(255) NOT NULL,
+    environment  varchar(255) NOT NULL,
+    content      CLOB         NOT NULL,
+    md5          varchar(32)           DEFAULT NULL,
+    gmt_create   timestamp    NOT NULL DEFAULT '2010-05-05 00:00:00',
+    gmt_modified timestamp    NOT NULL DEFAULT '2010-05-05 00:00:00',
+    config_desc  varchar(256)          DEFAULT NULL,
+    user_id      integer      NOT NULL,
+    username     varchar(255) NOT NULL,
 
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_configinfo_config_nameenvironment` (`config_name`, `environment`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin COMMENT ='config_info';
+    constraint configinfo_id_key PRIMARY KEY (id),
+    constraint uk_configinfo_confignameenvironment UNIQUE (config_name, environment)
+);
 
-CREATE TABLE `history_config_info`
+CREATE TABLE history_config_info
 (
-    `id`             bigint(64) unsigned NOT NULL AUTO_INCREMENT,
-    `config_info_id` bigint(20) unsigned NOT NULL,
-    `config_name`    varchar(255)        NOT NULL,
-    `environment`    varchar(128)        NOT NULL,
-    `content`        longtext            NOT NULL,
-    `md5`            varchar(32)  DEFAULT NULL,
-    `gmt_create`     bigint(15)          NOT NULL,
-    `gmt_modified`   bigint(15)          NOT NULL,
-    `src_user`       text,
-    `src_ip`         varchar(20)  DEFAULT NULL,
-    `op_type`        char(10)     DEFAULT NULL,
-    `tenant_id`      varchar(128) DEFAULT '' COMMENT '租户字段',
-    PRIMARY KEY (`id`),
-    KEY `idx_gmt_create` (`gmt_create`),
-    KEY `idx_gmt_modified` (`gmt_modified`),
-    KEY `idx_config_name` (`config_name`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin COMMENT ='配置历史记录表';
+    id          bigint(64) unsigned NOT NULL AUTO_INCREMENT,
+    config_name varchar(255) NOT NULL,
+    environment varchar(255) NOT NULL,
+    config_info CLOB         NOT NULL,
+    version     int          NOT NULL,
+    gmt_create  timestamp    NOT NULL DEFAULT '2010-05-05 00:00:00',
+    username    varchar(255) NOT NULL,
+
+    constraint history_config_info_key PRIMARY KEY (id),
+    constraint uk_history_config_info_confignameversion UNIQUE (config_name, version)
+);
 
 CREATE TABLE users
 (
@@ -70,6 +57,8 @@ CREATE TABLE permissions
     constraint uk_role_permission UNIQUE (role, resource, action)
 );
 
-INSERT INTO users (username, password, enabled) VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
+INSERT INTO users (username, password, enabled)
+VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
 
-INSERT INTO roles (username, role) VALUES ('nacos', 'ROLE_ADMIN');
+INSERT INTO roles (username, role)
+VALUES ('nacos', 'ROLE_ADMIN');
